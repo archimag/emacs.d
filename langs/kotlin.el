3a4175a4645ae9856d3d80e7e945733eb63ea18f
Moskvitin Andrey <archimag@gmail.com>
2018-12-03 14:06:17 +0300

    update kotlin.el

--- langs/kotlin.el
+++ langs/kotlin.el
@@ -8,16 +8,33 @@
 (setf gradle-use-gradlew t)
 (setf gradle-gradlew-executable "./gradlew")
 
-(add-to-list 'compilation-error-regexp-alist 'gradlew-error)
-(add-to-list 'compilation-error-regexp-alist 'gradlew-warn)
-(add-to-list 'compilation-error-regexp-alist-alist
-             '(gradlew-error "^e: \\(.+?\\): (\\([0-9]+\\), \\([0-9]+\\)):.*" 1 2 3))
-(add-to-list 'compilation-error-regexp-alist-alist
-             '(gradlew-warn "^w: \\(.+?\\): (\\([0-9]+\\), \\([0-9]+\\)):.*" 1 2 3 1))
-
 (eval-after-load 'flycheck
   (lambda ()
     (require 'flycheck-kotlin)
     (flycheck-kotlin-setup)))
+
+(require 'lsp-mode)
+
+(defun is-gradle-project-dir (dir)
+  "Check if DIR is contained build.gradle."
+  (file-exists-p (expand-file-name "build.gradle" dir)))
+
+(defun find-gradle-root-dir (&optional dir)
+  "Find proejct top directory base on current DIR."
+  (let ((root (locate-dominating-file (or dir default-directory) 'is-gradle-project-dir)))
+    (if root
+        (or (find-gradle-root-dir (file-name-directory (directory-file-name root)))
+            root))))
+
+(lsp-define-stdio-client lsp-kotlin-mode
+ "kotlin"
+ #'find-gradle-root-dir
+  '("kotlin-language-server"))
+
+(add-hook 'kotlin-mode-hook
+          (lambda ()
+            (flycheck-mode t)
+            (lsp-kotlin-mode-enable)))
+
 
 ;;; kotlin.el ends here
